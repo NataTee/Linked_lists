bc9406081657963e632e7d62a99ed35072c643ec
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-03-11 20:50:21 -0800

    added stub methods

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,61 @@
-/*
- * your comment header here
- */
+/***************************************************************************
+Name: Natalia Ksenz
+Coding 05
+Purpose: The assignment to create a fully functional doubly linked list class.
+
+File: linkedlist.cpp
+***************************************************************************/
+
+#include "linkedlist.h"
+
+LinkedList::LinkedList() {
+    head = NULL;
+}
+
+LinkedList::~LinkedList() {
+
+}
+
+bool LinkedList::addNode(int id, string *info) {
+    return;
+}
+
+bool LinkedList::deleteNode(int id) {
+    return;
+}
+
+bool LinkedList::getNode(int id, Data *data) {
+    return;
+}
+
+void LinkedList::printList(bool backward) {
+
+int LinkedList::getCount() {
+    int count;
+    return count;
+}
+
+bool LinkedList::clearList() {
+    return deleted;
+}
+
+bool LinkedList::exists(int id) {
+    bool exist;
+    return exist;
+}
+
+void LinkedList::addHead(Node* &head, Node* &newNode, bool &added) {
+
+}
+
+void LinkedList::addMiddle(Node* &current, Node* &newNode, bool &added) {
+
+}
+
+void LinkedList::addTail(Node* &current, Node* &newNode, bool &added) {
+
+}
+
+void LinkedList::fillIt(int id, string *info, Node* &newNode) {
+    
+}
